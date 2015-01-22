cbecefedfe1fef108a2a2263f34a6f7ae0eaea0d
alfred sang <i5ting@126.com>
2015-01-22 23:20:02 +0800

    refact

--- index.js
+++ index.js
@@ -8,51 +8,51 @@ function log(t){
 }
 
 function tpl_apply(template_path, data_obj, dest_file_path) {
-		var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
-		var bufferHelper = new BufferHelper();
+	var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
+	var bufferHelper = new BufferHelper();
 
-		rs.on("data", function (trunk){
-				bufferHelper.concat(trunk);
-		});
+	rs.on("data", function (trunk){
+			bufferHelper.concat(trunk);
+	});
+
+	rs.on("end", function () {
+		var source = bufferHelper.toBuffer().toString();
+		var template = Handlebars.compile(source);
 	
-		rs.on("end", function () {
-			var source = bufferHelper.toBuffer().toString();
-			var template = Handlebars.compile(source);
+		log(template);
 		
-			log(template);
-			
-			var content = template(data_obj);
+		var content = template(data_obj);
 
-			fs.writeFile(dest_file_path, content , function (err) {
-			  if (err) throw err;
-			  log('It\'s saved!');
-			});
-
+		fs.writeFile(dest_file_path, content , function (err) {
+		  if (err) throw err;
+		  log('It\'s saved!');
 		});
+
+	});
 };
 
 function tpl_apply_with_register_helper(Handlebars, template_path, data_obj, dest_file_path) {
-		var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
-		var bufferHelper = new BufferHelper();
+	var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
+	var bufferHelper = new BufferHelper();
 
-		rs.on("data", function (trunk){
-				bufferHelper.concat(trunk);
-		});
+	rs.on("data", function (trunk){
+			bufferHelper.concat(trunk);
+	});
+
+	rs.on("end", function () {
+		var source = bufferHelper.toBuffer().toString();
+		var template = Handlebars.compile(source);
 	
-		rs.on("end", function () {
-			var source = bufferHelper.toBuffer().toString();
-			var template = Handlebars.compile(source);
+		log(template);
 		
-			log(template);
-			
-			var content = template(data_obj);
+		var content = template(data_obj);
 
-			fs.writeFile(dest_file_path, content , function (err) {
-			  if (err) throw err;
-			  log('It\'s saved!');
-			});
-
+		fs.writeFile(dest_file_path, content , function (err) {
+		  if (err) throw err;
+		  log('It\'s saved!');
 		});
+
+	});
 };
 
 exports.tpl_apply_with_register_helper = tpl_apply_with_register_helper;
