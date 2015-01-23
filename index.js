344f10b1c9306c7020657081ef61eb0c50df7d67
alfred sang <i5ting@126.com>
2015-01-23 08:00:21 +0800

    code refact

--- index.js
+++ index.js
@@ -7,30 +7,17 @@ function log(t){
 	console.dir('[TPL_APPLY LOG] ' + t);
 }
 
+/**
+ * Basic
+ */ 
 function tpl_apply(template_path, data_obj, dest_file_path) {
-	var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
-	var bufferHelper = new BufferHelper();
-
-	rs.on("data", function (trunk){
-			bufferHelper.concat(trunk);
-	});
-
-	rs.on("end", function () {
-		var source = bufferHelper.toBuffer().toString();
-		var template = Handlebars.compile(source);
-	
-		log(template);
-		
-		var content = template(data_obj);
-
-		fs.writeFile(dest_file_path, content , function (err) {
-		  if (err) throw err;
-		  log('It\'s saved!');
-		});
-
-	});
+	tpl_apply_with_register_helper(Handlebars, template_path, data_obj, dest_file_path);
 };
 
+
+/**
+ * Custom with helpers
+ */ 
 function tpl_apply_with_register_helper(Handlebars, template_path, data_obj, dest_file_path) {
 	var rs = fs.createReadStream(template_path, {encoding: 'utf-8', bufferSize: 11}); 
 	var bufferHelper = new BufferHelper();
@@ -55,5 +42,5 @@ function tpl_apply_with_register_helper(Handlebars, template_path, data_obj, des
 	});
 };
 
-exports.tpl_apply_with_register_helper = tpl_apply_with_register_helper;
 exports.tpl_apply = tpl_apply;
+exports.tpl_apply_with_register_helper = tpl_apply_with_register_helper;
